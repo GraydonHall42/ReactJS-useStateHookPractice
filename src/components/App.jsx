273854251677ac4fc4c7b77728104569f4a58919
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-22 22:13:37 -0700

    finished practice challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 
 function App() {
+  //call function every second
   setInterval(updateTime, 1000);
 
+  //return current time
   const now = new Date().toLocaleTimeString();
 
+  //state variable = time
+  //setTime will be used to change state variable
+  //initial value is current time
   const [time, setTime] = useState(now);
 
+  //function to update state variable
   function updateTime() {
-    const newTime = new Date().toLocaleTimeString();
-    setTime(newTime);
+    const newTime = new Date().toLocaleTimeString(); //return current time
+    setTime(newTime); //update state variable
   }
 
   return (
